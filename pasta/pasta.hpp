dedad672b67fee04b9f58258aec297cb212746e8
BOYANG ZHANG <bzhang523@twhuang-desktop-06>
2026-01-27 17:52:52 -0600

    building up

--- pasta/pasta.hpp
+++ pasta/pasta.hpp
@@ -2,12 +2,57 @@
 
 #include <iostream>
 #include <string>
+#include "taskflow/taskflow.hpp"
+#include "wsq.hpp"
 
 namespace pasta {
 
 class Node;
 class Edge;
+class CNode;
+class CEdge;
 class Graph;
+
+class Node {
+
+  friend class Graph;
+
+  public:
+    Node(const std::string& name) : _name(name) {};
+
+    inline size_t num_fanins() const {
+      return _fanins.size();
+    }
+
+    inline size_t num_fanouts() const {
+      return _fanouts.size();
+    }
+
+    inline std::string name() {
+      return _name;
+    }
+
+  private:
+    std::string _name;  
+    std::list<Edge*> _fanins;
+    std::list<Edge*> _fanouts;
+    bool _visistead = false;
+    tf::Task _task;
+    int _cluster_id = -1; // specify which partition (cluster) it belongs
+    std::atomic<size_t> _dep_cnt{0};
+    CNode* _cnode = NULL; // specify which cnode (cluster) it belongs
+
+};
+
+class Edge {
+
+  friend class Graph;
+
+  private:
+    Node* _from;
+    Node* _to;
+
+};
 
 class Graph {
 
@@ -19,3 +64,29 @@ class Graph {
 };
 
 } // end of namespace pasta
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
