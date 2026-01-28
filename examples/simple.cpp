637b074dabed02b3d704a461330767b78456ce1c
BOYANG ZHANG <bzhang523@twhuang-desktop-06>
2026-01-27 18:01:47 -0600

    building up

--- examples/simple.cpp
+++ examples/simple.cpp
@@ -2,10 +2,31 @@
 
 int main() {
 
-  std::string filename = "../benchmarks/simple.txt";
+  pasta::Graph partitioner("../benchmarks/simple.txt"); 
 
-  pasta::Graph graph(filename);
+  // each task in the task graph is a NxN square matrix multiplication
+  size_t matrix_size = 8;  
+
+  if(!partitioner.has_cycle_before_partition()) {
+    partitioner.run_graph_before_partition(8);
+  }
+  else {
+    std::cerr << "input graph has cycle.\n";
+    std::exit(EXIT_FAILURE);
+  }
+
+  partitioner.set_partition_size(10);
+  partitioner.partition_c_pasta();
+  
+  if(!partitioner.has_cycle_after_partition()) {
+    partitioner.run_graph_after_partition(8);
+  }
+  else {
+    std::cerr << "partitioned graph has cycle.\n";
+    std::exit(EXIT_FAILURE);
+  }
+
+  // partitioner.dump_graph();
 
   return 0;
-
 }
--- pasta/pasta.hpp
+++ pasta/pasta.hpp
@@ -36,7 +36,7 @@ class Node {
     std::string _name;  
     std::list<Edge*> _fanins;
     std::list<Edge*> _fanouts;
-    bool _visistead = false;
+    bool _visited = false;
     tf::Task _task;
     int _cluster_id = -1; // specify which partition (cluster) it belongs
     std::atomic<size_t> _dep_cnt{0};
@@ -54,12 +54,79 @@ class Edge {
 
 };
 
+class CNode {
+
+  friend class Graph;
+
+  private:
+    bool _visited = false;
+    tf::Task _task;
+    std::list<Node*> _nodes;
+    std::list<CEdge*> _fanins;
+    std::list<CEdge*> _fanouts;
+
+};
+
+class CEdge {
+
+  friend class Graph;
+
+  private:
+    CNode* _from;
+    CNode* _to;
+
+};
+
 class Graph {
 
   public:
     // constructors
     Graph() {};
     Graph(const std::string& filename);
+
+     // basic ops
+    Node* insert_node(const std::string& name = "");
+    Edge* insert_edge(Node* from, Node* to);
+
+    // helper
+    inline size_t num_nodes() const {
+      return _nodes.size();
+    }
+    inline size_t num_edges() const {
+      return _edges.size();
+    }
+    inline void set_partition_size(const size_t partition_size) {
+      _partition_size = partition_size;
+    }
+    void dump_graph();
+
+    // check cycle
+    bool has_cycle_before_partition();
+    bool has_cycle_after_partition();
+
+    // C-PASTA
+    void partition_c_pasta();
+
+    // run graph with taskflow
+    void run_graph_before_partition(size_t task_runtime);
+    void run_graph_after_partition(size_t task_runtime);
+
+  private:
+
+    size_t _partition_size = 0;
+    int _max_cluster_id = -1; // record the largest cluster id
+
+    std::list<Node> _nodes;
+    std::list<Edge> _edges;
+    std::list<CNode> _cnodes;
+    std::list<CEdge> _cedges;
+
+    template <typename T>
+    void _topo_dfs(std::vector<T*>& topo_order, T* node);
+
+    void _assign_cluster_id(Node* node_ptr, std::vector<std::atomic<size_t>>& cluster_cnt, std::atomic<int>& max_cluster_id);
+
+    void _build_partitioned_graph();
 
 };
 
